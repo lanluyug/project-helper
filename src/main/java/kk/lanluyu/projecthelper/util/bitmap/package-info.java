df29946e19e3798c6b910672e294dd6d193045b1
zongzhihui <zongzhihui@mysteel.com>
2024-04-10 19:34:51 +0800

    新增BitMap

---
+++ src/main/java/kk/lanluyu/projecthelper/util/bitmap/package-info.java
@@ -0,0 +1,10 @@
+package kk.lanluyu.projecthelper.util.bitmap;
+
+/**
+ * copy from
+ *             <dependency>
+ *                 <groupId>cn.hutool</groupId>
+ *                 <artifactId>hutool-all</artifactId>
+ *                 <version>5.8.26</version>
+ *             </dependency>
+ */
