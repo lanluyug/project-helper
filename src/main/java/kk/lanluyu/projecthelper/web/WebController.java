e135fe5a152251b5cd03707e3a9b75f345a0716f
zongzhihui <zongzhihui@mysteel.com>
2024-04-26 13:33:11 +0800

    demo

---
+++ src/main/java/kk/lanluyu/projecthelper/web/WebController.java
@@ -0,0 +1,11 @@
+package kk.lanluyu.projecthelper.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/web")
+public class WebController {
+
+
+}
